9aed1ecd5e5ad9aa54a58a693a8b07746fcad50b
Michael Bernat <michael@hibernat.com>
2024-03-12 17:42:56 +0100

    potential bug demo

---
+++ ConcurrencyData/MainFile.swift
@@ -0,0 +1,57 @@
+//
+//  MainFile.swift
+//  ConcurrencyData
+//
+//  Created by Michael Bernat on 12.03.2024.
+//
+
+import Foundation
+import OSLog
+
+@main
+struct MainFile {
+    
+    static let logger = Logger()
+    
+    static func main() async {
+        do {
+            logger.info("Started")
+            try await run(maxTasksInGroup: 16)
+            logger.info("Finished")
+        } catch {
+            logger.error("Error: \(error)")
+        }
+    }
+    
+    static func run(maxTasksInGroup: Int) async throws {
+        await withTaskGroup(of: Result<Void, Error>.self) { group in
+            var tasksInGroup = 0
+            for _ in 0 ..< 1_000_000 {
+                let operation: @Sendable () async -> Result<Void, Error> = { @Sendable in
+                    async let result = Task {
+                        let data = try! Data(contentsOf: URL(filePath: "path-to/file.lzfse"))
+                        let _ = try (data as NSData).decompressed(using: .lzfse)
+                    }.result
+                    _ = await result
+                    return .success(())
+                }
+                if tasksInGroup < maxTasksInGroup {
+                    // there are still not enough concurrent tasks in the group
+                    group.addTask(operation: operation)
+                    tasksInGroup += 1
+                } else {
+                    // there are enough concurrent tasks in the group, must wait for finished task to add new task to the group
+                    _ = await group.next()
+                    group.addTask(operation: operation)
+                }
+            }
+            // not adding new tasks to the group any more, just waiting for finishing the group tasks
+            for await _ in group {
+                
+            }
+            // all tasks are finished
+            
+        }
+    }
+    
+}
